8b57bc3dbb9913de0aa8250898dd4bc4ba9a22a2
Sagar_Parekh <sagarparekh.sjp@gmail.com>
2019-08-23 13:37:06 -0700

    Organizational changes. Added autograd example

--- pytorch/learning_with_examples/tensors.py
+++ pytorch/learning_with_examples/tensors.py
@@ -11,10 +11,8 @@ N, D_in, H, D_out = 64, 1000, 100, 10
 
 # Create random input and output data
 # randn is for random numbers from a normal distribution with mean 0 and variance 1
-#
 x = torch.randn(N, D_in, device=device, dtype=dtype)
 y = torch.randn(N, D_out, device=device, dtype=dtype)
-
 
 # Randomly initialize weights
 w1 = torch.randn(D_in, H, device=device, dtype=dtype)
